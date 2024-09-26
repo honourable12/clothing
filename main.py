7c88effa82f2fc63264133cb8021401a26d8c8d0
honourable12 <honourablewise1@gmail.com>
2024-09-26 16:31:36 +0000

    fixtures

--- main.py
+++ main.py
@@ -825,5 +825,4 @@ async def get_sales_report(start_date: str, end_date: str, current_user: Employe
 
 if __name__ == "__main__":
 	import uvicorn
-
-	uvicorn.run("main:app", host = "127.0.0.1", port = 8000, reload = True)
+	uvicorn.run("main:app", host = "127.0.1.1", port = 8001, reload = True)
